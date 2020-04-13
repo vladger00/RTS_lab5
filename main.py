ef42fe7dc3acdb47a496783e56fe6dec8c90df55
vladger228 <fiot.vladislav.ger@gmail.com>
2020-04-13 16:13:32 +0300

    update

--- main.py
+++ main.py
@@ -12,47 +12,52 @@ class MyApp(kivy.app.App):
 
     def build(self):
         Window.clearcolor = (.88,.53,.0, 1)
-        self.textInput = kivy.uix.textinput.TextInput()
+        self.textInput1 = kivy.uix.textinput.TextInput()
+        self.textInput2 = kivy.uix.textinput.TextInput()
         bl = kivy.uix.boxlayout.BoxLayout (orientation = "vertical", padding = 50, spacing = 5)
         bl.add_widget(kivy.uix.label.Label(text = " Лабораторна робота №5. Факторизація Ферма"))
         bl.add_widget(kivy.uix.label.Label(text = "Введіть число n"))
-        bl.add_widget(self.textInput)
-        self.button = bl.add_widget(kivy.uix.button.Button(text="Факторизація",on_press=self.Message))
-        #bl.add_widget(self.button)
+        bl.add_widget(self.textInput1)
+        bl.add_widget(kivy.uix.label.Label(text="Введіть кількість ітерацій"))
+        bl.add_widget(self.textInput2)
+        self.button = bl.add_widget(kivy.uix.button.Button(text="Факторизація",on_press=self.factorization))
         self.label = kivy.uix.label.Label(text = "Результат")
         bl.add_widget(self.label)
 
         return bl
 
-    def Message(self, b):
-        txt = self.textInput.text
-        if txt.isdigit():
-            n = int(txt)
-            a,b = self.factorization(n)
-            self.label.text = "Для n : {}\n p = {}\n q = {}".format(n, a, b)
-        else:
-            self.label.text = "Введіть число"
-
     def factorization(self, n):
         print(n)
-        s = math.ceil(math.sqrt(n))
-        k = 0
-        while True:
-            x = s + k
-            y2 = x ** 2 - n
-            y = int(math.sqrt(y2))
-            if y * y == y2:
-                p = x + y
-                q = x - y
-                break
-            elif x >= n :
-                p, q = None, None
-                break
+        txt = self.textInput1.text
+        numb = self.textInput2.text       #Число ітерацій, задане користувачем
+        if txt.isdigit() and numb.isdigit():
+            numb = int(numb)
+            n= int(txt)
+            s = math.ceil(math.sqrt(n))
+            k = 0
+            while True:
+                if k < numb:
+                    x = s + k
+                    y2 = x ** 2 - n
+                    y = int(math.sqrt(y2))
+                    if y * y == y2:
+                        p = x + y
+                        q = x - y
+                        self.label.text = "Для n : {}\n p = {}\n q = {}".format(n, p, q)
+                        break
+                    elif x >= n :
+                        p, q = None, None
+                        self.label.text = "Для n : {}\n p = {}\n q = {}".format(n, p, q)
+                        break
+                else:
+                    self.label.text = "Помилка. Ввведена кількість ітерацій перевищена"
+                    break
 
-            k += 1
+                k += 1
+        else:
+            self.label.text = "Введіть число n та кількість ітерацій"
 
 
-        return p, q
 
 if __name__ == "__main__":
     app = MyApp()
